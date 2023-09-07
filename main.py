ccdbfc3c8147f89815bced954ada581a087d7cbc
Stephen Adegbokun <obaadegbokun1@gmail.com>
2023-09-07 22:33:47 +0100

    feat: fixed datetime format

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def return_details(slack_name: str, track: str):
     res = {
         "slack_name": slack_name,
         "current_day": datetime.now().strftime("%A"),
-        "utc_time": datetime.utcnow(),
+        "utc_time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
         "track": track,
         "github_file_url": "https://github.com/datalordstephen/hng-task-1/blob/master/main.py",
         "github_repo_url": "https://github.com/datalordstephen/hng-task-1",
