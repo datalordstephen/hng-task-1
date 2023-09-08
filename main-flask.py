895c7210b0526e8c8abff8c12adec69aa0537878
Stephen Adegbokun <obaadegbokun1@gmail.com>
2023-09-08 22:06:08 +0100

    feat: did it with flask too

---
+++ main-flask.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/api', methods=["GET"])
+def return_details():
+    if request.method == "GET":
+        slack_name = request.args.get("slack_name", None)
+        track = request.args.get("track", None)
+    
+        res = {
+            "slack_name": slack_name,
+            "current_day": datetime.now().strftime("%A"),
+            "utc_time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "track": track,
+            "github_file_url": "https://github.com/datalordstephen/hng-task-1/blob/master/main.py",
+            "github_repo_url": "https://github.com/datalordstephen/hng-task-1",
+            "status_code": 200
+        }
+        
+        return res
